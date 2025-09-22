2d2b58cdc2cbed0773c2b14ba0a92059753e3b84
Rama Krishna <raamakrishna7729@gmail.com>
2025-09-22 16:29:00 +0530

    I done my side

--- src/main/java/com/driver/StudentRepository.java
+++ src/main/java/com/driver/StudentRepository.java
@@ -19,40 +19,60 @@ public class StudentRepository {
 
     public void saveStudent(Student student){
         // your code goes here
+        studentMap.put(student.toString(),student);
     }
 
     public void saveTeacher(Teacher teacher){
         // your code goes here
+        teacherMap.put(teacher.toString(),teacher);
     }
 
     public void saveStudentTeacherPair(String student, String teacher){
         if(studentMap.containsKey(student) && teacherMap.containsKey(teacher)){
             // your code goes here
+            List<String> temp = new ArrayList<String>();
+            temp.add(student.toString());
+            temp.add(teacher.toString());
+            teacherStudentMapping.put(temp.toString(),temp);
         }
     }
 
     public Student findStudent(String student){
         // your code goes here
+        return studentMap.get(student);
     }
 
     public Teacher findTeacher(String teacher){
         // your code goes here
+        return teacherMap.get(teacher);
     }
 
     public List<String> findStudentsFromTeacher(String teacher){
         // your code goes here
         // find student list corresponding to a teacher
+        List<String> res =new ArrayList<>();
+        HashMap<String, List<String>> temp = teacherStudentMapping;
+        for(List<String> temp2:teacherStudentMapping.values()){
+            if(teacher.equalsIgnoreCase(temp2.get(1))){
+                res.add(temp2.get(0));
+            }
+        }
+        return res;
     }
 
     public List<String> findAllStudents(){
         // your code goes here
+        List<String> res = (List<String>) studentMap.keySet();
+        return res;
     }
 
     public void deleteTeacher(String teacher){
         // your code goes here
+        teacherMap.remove(teacher);
     }
 
     public void deleteAllTeachers(){
         // your code goes here
+        teacherMap.clear();
     }
 }
